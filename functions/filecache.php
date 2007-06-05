64f7a95dabebfcd9f19a1cdd59f7fd1a4962f07e
jasonpell <jasonpell@users.sf.net>
2007-06-05 20:10:36 +0000

    revert change that set temporary directory to filecache.  Want to investigate this further first.
    git-svn-id: svn://svn.code.sf.net/p/opendb/code/opendb/branches/BRANCH_1_0@19 2c52d18a-8f90-4aa0-bf14-52c5ce5e299b

--- functions/filecache.php
+++ functions/filecache.php
@@ -194,7 +194,7 @@ function file_cache_save_thumbnail_file($file_cache_r, &$errors)
 				$phpThumb = new phpThumb();
 				
 				// prevent issues with safe mode and /tmp directory
-				$phpThumb->config_temp_directory = './itemcache';
+				//$phpThumb->config_temp_directory = './itemcache';
 				
 				$phpThumb->setParameter('config_error_die_on_error', FALSE);
 				//$phpThumb->setParameter('config_prefer_imagemagick', FALSE);

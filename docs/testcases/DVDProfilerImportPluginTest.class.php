131a4df8a09224071ea41475de78e9806044ff5d
jasonpell <jasonpell@users.sf.net>
2007-10-13 14:10:25 +0000

    todo entry added
    git-svn-id: svn://svn.code.sf.net/p/opendb/code/opendb/trunk@253 2c52d18a-8f90-4aa0-bf14-52c5ce5e299b

--- docs/testcases/DVDProfilerImportPluginTest.class.php
+++ docs/testcases/DVDProfilerImportPluginTest.class.php
@@ -18,6 +18,8 @@
 	Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 */
 
+// TODO - add tests
+
 require_once 'PHPUnit.php';
 
 require_once("./functions/StringFileHandler.class.php");

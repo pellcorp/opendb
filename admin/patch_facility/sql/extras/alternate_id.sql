59c595121ec6ad20819e98c1fdb4856f691751b4
jasonpell <jasonpell@users.sf.net>
2007-10-29 13:10:21 +0000

    make alternate id a instance attribute
    git-svn-id: svn://svn.code.sf.net/p/opendb/code/opendb/trunk@320 2c52d18a-8f90-4aa0-bf14-52c5ce5e299b

--- admin/patch_facility/sql/extras/alternate_id.sql
+++ admin/patch_facility/sql/extras/alternate_id.sql
@@ -3,12 +3,12 @@
 #
 INSERT INTO s_attribute_type ( s_attribute_type, description, prompt, input_type, input_type_arg1, input_type_arg2, input_type_arg3, input_type_arg4, input_type_arg5, display_type, display_type_arg1, display_type_arg2, display_type_arg3, display_type_arg4, display_type_arg5, listing_link_ind, file_attribute_ind, lookup_attribute_ind, multi_attribute_ind, s_field_type, site_type ) VALUES ( 'ALT_ID', 'Alternate Item ID', 'Alt Item ID', 'text', '10', NULL, NULL, NULL, NULL, 'display','%value%', NULL, NULL, NULL, NULL, 'N', 'N', 'N', 'N', NULL, NULL);
 
-INSERT INTO s_item_attribute_type (s_item_type, s_attribute_type, order_no, prompt, compulsory_ind) VALUES ( 'DVD', 'ALT_ID', '1', NULL, 'N');
-INSERT INTO s_item_attribute_type (s_item_type, s_attribute_type, order_no, prompt, compulsory_ind) VALUES ( 'CD', 'ALT_ID', '1', NULL, 'N');
-INSERT INTO s_item_attribute_type (s_item_type, s_attribute_type, order_no, prompt, compulsory_ind) VALUES ( 'LD', 'ALT_ID', '1', NULL, 'N');
-INSERT INTO s_item_attribute_type (s_item_type, s_attribute_type, order_no, prompt, compulsory_ind) VALUES ( 'MP3', 'ALT_ID', '1', NULL, 'N');
-INSERT INTO s_item_attribute_type (s_item_type, s_attribute_type, order_no, prompt, compulsory_ind) VALUES ( 'VHS', 'ALT_ID', '1', NULL, 'N');
-INSERT INTO s_item_attribute_type (s_item_type, s_attribute_type, order_no, prompt, compulsory_ind) VALUES ( 'GAME', 'ALT_ID', '1', NULL, 'N');
-INSERT INTO s_item_attribute_type (s_item_type, s_attribute_type, order_no, prompt, compulsory_ind) VALUES ( 'BOOK', 'ALT_ID', '1', NULL, 'N');
-INSERT INTO s_item_attribute_type (s_item_type, s_attribute_type, order_no, prompt, compulsory_ind) VALUES ( 'SVCD', 'ALT_ID', '1', NULL, 'N');
-INSERT INTO s_item_attribute_type (s_item_type, s_attribute_type, order_no, prompt, compulsory_ind) VALUES ( 'DIVX', 'ALT_ID', '1', NULL, 'N');
+INSERT INTO s_item_attribute_type (s_item_type, s_attribute_type, order_no, prompt, compulsory_ind, instance_attribute_ind) VALUES ( 'DVD', 'ALT_ID', '1', NULL, 'N', 'Y');
+INSERT INTO s_item_attribute_type (s_item_type, s_attribute_type, order_no, prompt, compulsory_ind, instance_attribute_ind) VALUES ( 'CD', 'ALT_ID', '1', NULL, 'N', 'Y');
+INSERT INTO s_item_attribute_type (s_item_type, s_attribute_type, order_no, prompt, compulsory_ind, instance_attribute_ind) VALUES ( 'LD', 'ALT_ID', '1', NULL, 'N', 'Y');
+INSERT INTO s_item_attribute_type (s_item_type, s_attribute_type, order_no, prompt, compulsory_ind, instance_attribute_ind) VALUES ( 'MP3', 'ALT_ID', '1', NULL, 'N', 'Y');
+INSERT INTO s_item_attribute_type (s_item_type, s_attribute_type, order_no, prompt, compulsory_ind, instance_attribute_ind) VALUES ( 'VHS', 'ALT_ID', '1', NULL, 'N', 'Y');
+INSERT INTO s_item_attribute_type (s_item_type, s_attribute_type, order_no, prompt, compulsory_ind, instance_attribute_ind) VALUES ( 'GAME', 'ALT_ID', '1', NULL, 'N', 'Y');
+INSERT INTO s_item_attribute_type (s_item_type, s_attribute_type, order_no, prompt, compulsory_ind, instance_attribute_ind) VALUES ( 'BOOK', 'ALT_ID', '1', NULL, 'N', 'Y');
+INSERT INTO s_item_attribute_type (s_item_type, s_attribute_type, order_no, prompt, compulsory_ind, instance_attribute_ind) VALUES ( 'SVCD', 'ALT_ID', '1', NULL, 'N', 'Y');
+INSERT INTO s_item_attribute_type (s_item_type, s_attribute_type, order_no, prompt, compulsory_ind, instance_attribute_ind) VALUES ( 'DIVX', 'ALT_ID', '1', NULL, 'N', 'Y');

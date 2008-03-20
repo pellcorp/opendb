d458270b39f07e43fae6f64be1ef6b2ea39aaf47
jasonpell <jasonpell@users.sf.net>
2008-03-20 12:24:45 +0000

    update michaeld to support dvd audio attribute instead of audio_lang
    git-svn-id: svn://svn.code.sf.net/p/opendb/code/opendb/trunk@665 2c52d18a-8f90-4aa0-bf14-52c5ce5e299b

--- site/michaeld.class.php
+++ site/michaeld.class.php
@@ -240,11 +240,11 @@ class michaeld extends SitePlugin
 				}
 	
 				if(preg_match('#english[^,]dolby digital 5\.1#i', $matches[1]))
-					$this->addItemAttribute('audio_lang', 'ENGLISH_5.1');
+					$this->addItemAttribute('dvd_audio', 'ENGLISH_5.1');
 				if(preg_match('#english dts#i', $matches[1]))
-					$this->addItemAttribute('audio_lang', 'ENGLISH_DTS');
+					$this->addItemAttribute('dvd_audio', 'ENGLISH_DTS');
 				if(preg_match('#commentary#i', $matches[1]))
-					$this->addItemAttribute('audio_lang', 'DIR_COMMENT');
+					$this->addItemAttribute('dvd_audio', 'DIR_COMMENT');
 			}
 	
 			if (preg_match('#<!--\s*audio parameters.*?</tr.*?subtitles.*?</tr>.*?<tr.*?>(.*?)</tr#is',$page, $matches))

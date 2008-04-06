ef867a4121713f02a6bdb8b5f9ec7ad20ceb9b12
jasonpell <jasonpell@users.sf.net>
2008-04-06 05:10:52 +0000

    accidentally committed trunk to 1.5.0_B2
    git-svn-id: svn://svn.code.sf.net/p/opendb/code/opendb/tags/RELEASE_1_5_0_B2@694 2c52d18a-8f90-4aa0-bf14-52c5ce5e299b

--- trunk/install/upgrade/1.5.0a1/step1.sql
+++
@@ -1,6 +0,0 @@
-# Bug 1885700 - remove OTHER item type group
-DELETE FROM s_item_type_group WHERE s_item_type_group = 'OTHER';
-DELETE FROM s_item_type_group_rltshp WHERE s_item_type_group = 'OTHER';
-
-# Drop system indicator column as no longer makes any sense
-ALTER TABLE s_item_type_group DROP system_ind;

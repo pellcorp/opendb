314d29099a089e3385c7c71e88b7f414397d4a1a
jasonpell <jasonpell@users.sf.net>
2008-07-07 23:44:48 +0000

    include a LYNX_HOME var - so does not have to be in path on windows
    git-svn-id: svn://svn.code.sf.net/p/opendb/code/opendb/trunk@715 2c52d18a-8f90-4aa0-bf14-52c5ce5e299b

--- docs/scripts/reminders.bat
+++ docs/scripts/reminders.bat
@@ -1,3 +1,4 @@
+set LYNX_HOME="c:\Program Files\Lynx\"
 set OPENDB_URL=http://opendb.iamvegan.net/demo1.0
 set OPENDB_ADMIN_UID=admin
 set OPENDB_ADMIN_PWD=admin
@@ -5,7 +6,7 @@ set OPENDB_ADMIN_PWD=admin
 set LOGFILE=c:\OpenDb_Reminder.log
 set OPENDB_ADMIN_EMAIL=opendb@iamvegan.net
 
-lynx -accept_all_cookies -dump -dont_wrap_pre "%OPENDB_URL%/login.php?op=login&uid=%OPENDB_ADMIN_UID%&passwd=%OPENDB_ADMIN_PWD%&redirect=item_borrow.php?op%3Dadmin_send_reminders%26mode%3Djob" > %LOGFILE%
+%LYNX_HOME%\lynx -accept_all_cookies -dump -dont_wrap_pre "%OPENDB_URL%/login.php?op=login&uid=%OPENDB_ADMIN_UID%&passwd=%OPENDB_ADMIN_PWD%&redirect=item_borrow.php?op%3Dadmin_send_reminders%26mode%3Djob" > %LOGFILE%
 
 REM mail -s "Borrower Reminders Log `date +%d/%m/%y`" %OPENDB_ADMIN_EMAIL% < %LOGFILE%
 REM rm %LOGFILE%

952bfa0c9b98a8aa934d2fb485f9a8de15239d5a
jasonpell <jasonpell@users.sf.net>
2008-07-18 22:44:00 +0000

    Add moviemeter.nl to http streaming images support.
    git-svn-id: svn://svn.code.sf.net/p/opendb/code/opendb/trunk@733 2c52d18a-8f90-4aa0-bf14-52c5ce5e299b

--- install/upgrade/1.5.0b5/step1.sql
+++ install/upgrade/1.5.0b5/step1.sql
@@ -25,3 +25,6 @@ INSERT INTO s_attribute_type_lookup ( s_attribute_type, order_no, value, display
 
 INSERT INTO s_attribute_type_lookup ( s_attribute_type, order_no, value, display, img, checked_ind ) VALUES ( 'MOVIEGENRE', NULL, 'Parody', '', '', 'N' );
 INSERT INTO s_attribute_type_lookup ( s_attribute_type, order_no, value, display, img, checked_ind ) VALUES ( 'MOVIEGENRE', NULL, 'Television', '', '', 'N' );
+
+# MovieMeter support
+INSERT INTO s_config_group_item_var ( group_id, id, keyid, value ) VALUES ('http.stream_external_images', 'domain_list', '3', 'moviemeter.nl');

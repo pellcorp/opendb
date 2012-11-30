7896aed717343fc64486bec33b96f3f628e14dc1
jasonpell <jasonpell@users.sf.net>
2012-11-30 13:15:05 +0000

    remove uncessary code from tests
    git-svn-id: svn://svn.code.sf.net/p/opendb/code/opendb/trunk@993 2c52d18a-8f90-4aa0-bf14-52c5ce5e299b

--- docs/test/test_thumbnails.php
+++ docs/test/test_thumbnails.php
@@ -41,8 +41,6 @@ if(@$_GET['op'] == 'generate')
 	$phpThumb->setParameter('f', 'jpeg');
 	$phpThumb->setParameter('config_output_format', 'jpeg');
 	$phpThumb->setParameter('config_cache_directory', '/tmp');
-	$phpThumb->setParameter('config_prefer_imagemagick', FALSE);
-	$phpThumb->setParameter('config_imagemagick_use_thumbnail', FALSE);
 	
 	$phpThumb->setSourceFilename(realpath('./itemcache/5.cache.jpeg'));
 	

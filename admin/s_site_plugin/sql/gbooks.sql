d8f88b72cff4e9a7d84c66fd4928ea3844b0792c
Esben Madsen <github.com@minpingvin.dk>
2013-06-16 22:38:00 +0200

    Google Books site plugin

---
+++ admin/s_site_plugin/sql/gbooks.sql
@@ -0,0 +1,49 @@
+/usr/bin/curl
+#########################################################
+# OpenDb 1.5.0.8RC2 Google Books (gbooks) Site Plugin
+#########################################################
+
+#
+# Site Plugin.
+#
+
+INSERT INTO s_site_plugin ( site_type, classname, title, image, description, external_url, items_per_page, more_info_url )VALUES ( 'gbooks', 'gbooks', 'Google Books', 'gbooks_logo.png', 'Google Books has a large database of books to search, read online and buy', 'http://books.google.com', 10, 'http://books.google.com/books?id={gbooks_id}' );
+
+#
+# Site Plugin Input Fields
+#
+
+INSERT INTO s_site_plugin_input_field ( site_type, field, order_no, description, prompt, field_type, default_value, refresh_mask ) VALUES ( 'gbooks', 'title', 1, '', 'Title Search', 'text', '', '{title}' );
+INSERT INTO s_site_plugin_input_field ( site_type, field, order_no, description, prompt, field_type, default_value, refresh_mask ) VALUES ( 'gbooks', 'author', 2, '', 'Author', 'text', '', '{author}' );
+INSERT INTO s_site_plugin_input_field ( site_type, field, order_no, description, prompt, field_type, default_value, refresh_mask ) VALUES ( 'gbooks', 'isbn', 3, '', 'ISBN', 'text', '', '{isbn}' );
+INSERT INTO s_site_plugin_input_field ( site_type, field, order_no, description, prompt, field_type, default_value, refresh_mask ) VALUES ( 'gbooks', 'gbooks_id', 4, '', 'ID Number', 'hidden', '', '{gbooks_id}' );
+
+#
+# Site Plugin Links
+#
+
+INSERT INTO s_site_plugin_link ( site_type, s_item_type_group, s_item_type, order_no, description, url, title_url ) VALUES ( 'gbooks', '*', 'BOOK', 1, 'More Info', 'http://books.google.dk/books?id={gbooks_id}', '' );
+
+#
+# Site Plugin System Attribute Type Map
+#
+
+INSERT INTO s_site_plugin_s_attribute_type_map ( site_type, s_item_type_group, s_item_type, variable, s_attribute_type, lookup_attribute_val_restrict_ind ) VALUES ( 'gbooks', '*', '*', 'genre', 'BOOKGENRE', 'Y' );
+INSERT INTO s_site_plugin_s_attribute_type_map ( site_type, s_item_type_group, s_item_type, variable, s_attribute_type, lookup_attribute_val_restrict_ind ) VALUES ( 'gbooks', '*', '*', 'title', 'ALT_TITLE', 'N' );
+INSERT INTO s_site_plugin_s_attribute_type_map ( site_type, s_item_type_group, s_item_type, variable, s_attribute_type, lookup_attribute_val_restrict_ind ) VALUES ( 'gbooks', '*', '*', 'title', 'S_TITLE', 'N' );
+
+####################################################################################################
+# Item Type / Attribute Type relationships
+####################################################################################################
+
+#
+# Site Plugin Attribute Type(s)
+#
+
+INSERT INTO s_attribute_type ( s_attribute_type, description, prompt, input_type, display_type, s_field_type, site_type ) VALUES ( 'GBOOKS_ID', 'Google Books ID', 'Google Books ID', 'text', 'hidden', '', 'gbooks' );
+
+#
+# Site Plugin Item Attribute Type Relationship(s)
+#
+
+INSERT INTO s_item_attribute_type ( s_item_type, s_attribute_type, order_no, prompt, compulsory_ind ) VALUES ( 'BOOK', 'GBOOKS_ID',  0, '', 'N' );
---
+++ lib/site/gbooks.class.php
@@ -0,0 +1,140 @@
+<?php
+/* 	
+    Open Media Collectors Database
+    Copyright (C) 2001,2013 by Jason Pell
+
+	Google books site plugin
+	Copyright (C) 2013 by Esben Madsen
+    This program is free software; you can redistribute it and/or
+    modify it under the terms of the GNU General Public License
+    as published by the Free Software Foundation; either version 2
+    of the License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+    Change Information
+    -------------------
+
+    0.1 Initial version
+
+ */
+include_once("./lib/SitePlugin.class.php");
+
+class gbooks extends SitePlugin {
+	
+	var $base_url='https://www.googleapis.com/books/v1/volumes';
+	
+	function gbooks($site_type) {
+		parent::SitePlugin($site_type);
+	}
+
+	function queryListing($page_no, $items_per_page, $offset, $s_item_type, $search_vars_r) {
+		// standard block of code to cater for refresh option, where item already has
+		// reference to site item unique ID.
+		if (strlen($search_vars_r['gbooks_id']) > 0) {
+			$this->addListingRow(NULL, NULL, NULL, array('gbooks_id' => $search_vars_r['gbooks_id']));
+			return TRUE;
+		}
+
+// compose search query - Google Books API has the following definitions:
+/* q - Search for volumes that contain this text string. 
+ * There are special keywords you can specify in the search terms to search in particular fields, such as:
+ *   intitle:     Returns results where the text following this keyword is found in the title.
+ *   inauthor:    Returns results where the text following this keyword is found in the author.
+ *   inpublisher: Returns results where the text following this keyword is found in the publisher.
+ *   subject:     Returns results where the text following this keyword is listed in the category list of the volume.
+ *   isbn:        Returns results where the text following this keyword is the ISBN number.
+ *   lccn:        Returns results where the text following this keyword is the Library of Congress Control Number.
+ *   oclc:        Returns results where the text following this keyword is the Online Computer Library Center number.
+ */
+
+		$search_query = array();
+		if (strlen($search_vars_r['author']) > 0)
+			$search_query[] = 'inauthor:'.urlencode('"'.$search_vars_r['author'].'"');
+		if (strlen($search_vars_r['title']) > 0)
+			$search_query[] = 'intitle:'.urlencode('"'.$search_vars_r['title'].'"');
+		if (strlen($search_vars_r['isbn']) > 0)
+			$search_query[] = 'isbn:'.urlencode($search_vars_r['isbn']);
+		
+		// utf8decode because sitePlugin.class.php wrongly encodes what is already utf8 from google
+		$result_json = utf8_decode($this->fetchURI($this->base_url . '?q=' . join('+',$search_query)));
+		$result = json_decode($result_json,true);		
+
+// Get number of matches
+		if(isset($result['totalItems'])&& ($result['totalItems'] > 0) ) {
+			foreach($result['items'] as $item) {
+				$vol = $item['volumeInfo'];
+				$title ='';
+					isset($vol['title'])		&& $title = $vol['title'] . ' ';
+					isset($vol['subtitle'])	&& $title .= '- '. $vol['subtitle'] . ' ';
+					isset($vol['publishedDate'])	&& $title .= '('.$vol['publishedDate'] . ') ';
+					isset($vol['authors'])	&& $title .= 'by ' . join('; ', $vol['authors']);
+				$cover_image_url = (isset($vol['imageLinks']) ? $vol['imageLinks']['thumbnail'] : NULL );
+				$comments='';
+					if(isset($vol['industryIdentifiers'])){
+						$iids = array();
+						foreach($vol['industryIdentifiers'] as $iid){
+							$iids[] = $iid['type'] .': '. $iid['identifier'];
+						}
+						$comments = join(', ',$iids);
+					}
+				$attributes_r=array('gbooks_id' => $item['id']);
+				$this->addListingRow($title, $cover_image_url, $comments, $attributes_r);
+			}
+		}
+		return TRUE;
+	}
+
+	function queryItem($search_attributes_r, $s_item_type) {
+		// utf8decode because sitePlugin.class.php wrongly encodes what is already utf8 from google
+		$result_json = utf8_decode($this->fetchURI($this->base_url . '/' . $search_attributes_r['gbooks_id']));
+		$result = json_decode($result_json,true);		
+//echo('GBOOKS: '.print_r($result,true).'<br/>');
+
+		// make sure we actually got data
+		if(!isset($result['id'])) {
+			return FALSE;
+		}
+
+		$vol = $result['volumeInfo'];
+
+		
+		$title ='';
+			isset($vol['title'])	&& $title = $vol['title'] . ' ';
+			isset($vol['subtitle'])	&& $title .= '- '. $vol['subtitle'] . ' ';
+		$this->addItemAttribute('title', $title);	
+		
+		isset($vol['publishedDate']) && $this->addItemAttribute('pub_date', $vol['publishedDate']);
+		
+		isset($vol['authors']) && $this->addItemAttribute('author', $vol['authors']);
+		
+		
+		isset($vol['imageLinks']) &&  $this->addItemAttribute('imageurl', $vol['imageLinks']['thumbnail']) ;
+		
+		
+		if(isset($vol['industryIdentifiers'])){
+			foreach($vol['industryIdentifiers'] as $iid){
+				$iid['type'] == "ISBN_10" && $this->addItemAttribute('isbn', $iid['identifier']);
+				$iid['type'] == "ISBN_13" && $this->addItemAttribute('isbn13', $iid['identifier']);
+			}
+		}
+
+		isset($vol['categories']) && $this->addItemAttribute('genre', $vol['categories']);
+
+		isset($vol['description']) && $this->addItemAttribute('synopsis', $vol['description']);
+				
+		isset($vol['publisher']) && $this->addItemAttribute('publisher', $vol['publisher']);
+
+		isset($vol['pageCount']) && $this->addItemAttribute('no_pages', $vol['pageCount']);
+				
+		return TRUE;
+	}
+}
+?>

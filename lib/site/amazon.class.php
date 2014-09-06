729d59902d5075d984b860ff72997e3a33613610
Jason Pell <jason@pellcorp.com>
2014-09-06 22:36:50 +1000

    Further work on issue #66

--- lib/site/amazon.class.php
+++ lib/site/amazon.class.php
@@ -63,7 +63,7 @@ class amazon extends SitePlugin {
 					$amazonasin = trim($regs[1]);
 				} else if (preg_match_all("!<div id=\"result_([0-9]+)\"!", $pageBuffer, $regs)) {
 					if (count($regs[0]) == 1) {
-						if (preg_match("!<a href=\".*?/dp/([0-9]+)/.*?keywords=.*?!", $pageBuffer, $regs)) {
+						if (preg_match("!<a href=\".*?/dp/([^/]+)/.*?keywords=.*?!", $pageBuffer, $regs)) {
 							$amazonasin = trim($regs[1]);
 						}
 					}
